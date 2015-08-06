0850ce393de4eaf682c3f07f8651811bc4900f06
Ovais Tariq <ovaistariq@gmail.com>
2015-08-05 18:02:27 -0700

    Add proper python package name in requirements and setup file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("HISTORY.rst") as history_file:
 
 requirements = [
     # TODO: put package requirements here
-    "mysql.connector"
+    "mysql-connector-python"
 ]
 
 test_requirements = [
